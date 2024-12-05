086650f9e5bf08d2badac8772b6a1ca327ac97b1
Ilya Obolenskiy <iobolenskiy@avito.ru>
2024-12-05 21:41:55 +0300

    Webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
   },
 };
